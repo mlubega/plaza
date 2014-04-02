f7bf6ba3442377deb88b243b43c24d87ed7bf97b
Maimuna Lubega <maimuna.lubega@gmail.com>
2014-04-01 22:40:15 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plaza::Application.config.secret_key_base = '766d5df4ba63584a22855db34122ec742141373d79b621699fba339188b20beb917a540d94735e02b282e381ef4b3c3e4f2e2af95c58534f9e3f5ebd7f1f73f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Plaza::Application.config.session_store :cookie_store, key: '_plaza_session'
