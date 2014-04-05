572b3092734e5e06150809349dc1b9054b6ff781
Fan <fzheng3@wisc.edu>
2014-04-04 22:55:59 -0500

    write 3 tests

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    title   "Example Post"
+    content "This is an example post."
+  end
+end
---
+++ spec/requests/post_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Post pages" do
+  subject { page }
+  describe "posts" do
+    before { visit posts_path }
+    it { should have_content('Posts List') } 
+  end
+  describe "create post" do
+    before { visit new_post_path }
+    it { should have_content('New Post')}
+    
+    it "should go to posts lists" do
+      click_button "Post!"
+      expect(page).to have_content('Posts List') 
+    end
+  end
+end
