08ae836de4face883503e352c3da81a5795c5f0f
Fan <fzheng3@wisc.edu>
2014-05-02 01:04:42 -0500

    Sign up done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Sign up successfully! Welcome to Plaza!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -7,4 +18,10 @@ class UsersController < ApplicationController
 
   def edit
   end
+  
+  private
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+    end
 end
