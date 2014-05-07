f709f48af69ae520e2d2d21923aa4c03a4b1c5de
Fan <fzheng3@wisc.edu>
2014-05-07 09:51:27 -0500

    down case user email in session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Sign up successfully! Welcome to Plaza!"
+      flash[:success] = "Sign up successfully! Welcome to Plaza #{@user.name}!"
       redirect_to controller:'static_pages', action:'home'
     else
       render 'new'
